14766ac1bef3ce2663b748ae4720b06750a7ddd9
Alina Povazhnyuk <61550506+HtwwtH@users.noreply.github.com>
2020-10-03 20:48:31 +0700

    Update mobile_menu.js

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   const mMenuBtn = $(".about__m-menu-button");
   const mMenu = $(".mobile-menu");
-  const tab = $(".tab");
 
   mMenuBtn.on("click", function () {
     mMenu.toggleClass("active");
